096176561b58821398a022ed10809303640ea9a1
Allan70 <aabere70@gmail.com>
2022-10-16 12:35:07 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,6 +2,13 @@
 
 int main(void)
 {
-	putchar();
+	char arr[25];
+
+	arr[] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','l','o','p','q','r','s','t','u','v','w','x','y','z'}
+
+	for (int i = 0; i < sizeof(arr); i++)
+	{
+		putchar(arr[i]);
+	}
 	return (0);
 }
