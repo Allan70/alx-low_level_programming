17e937d475ac1731e55bcb4d232cc3a4723ee246
Allan70 <aabere70@gmail.com>
2022-10-16 12:38:55 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,7 +2,7 @@
 
 int main(void)
 {
-	char arr[] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','l','o','p','q','r','s','t','u','v','w','x','y','z'}
+	char arr[] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','l','o','p','q','r','s','t','u','v','w','x','y','z'};
 
 	for (int i = 0; i < sizeof(arr); i++)
 	{
