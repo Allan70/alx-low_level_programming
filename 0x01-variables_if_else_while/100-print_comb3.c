23ab8cd7b0a016e5526a5e843ce40da208d741eb
Allan70 <aabere70@gmail.com>
2022-10-17 00:41:33 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,17 +11,13 @@ int main(void)
 	int number;
 	int ourNumber;
 
-	//first number
 	for (number = '0'; number <= '9'; number++)
 	{
-		// get first number /10
 		putchar(number)
-
 		for (ourNumber = '0'; number <= '9'; number++)
 		{
 			if (number < ourNumber)
 			{
-			// get second number %10
 			putchar(ourNumber);
 			}
 
