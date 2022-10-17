94286ef404f5248f98606ee51a8180de6204a7fd
Allan70 <aabere70@gmail.com>
2022-10-17 01:40:55 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -12,23 +12,28 @@ int main(void)
 	int number2;
 	int number3;
 
+	number3 = '0';
+
 	for (number1 = '0'; number1 < '8'; number1++)
 		for (number2 = '0'; number2 < '9'; number2++)
-			for (number3 = '0'; number3 <= '9'; number3++)
+			while (number3 <= '9')
+			{
 				if (number1 < number2 && number2 < number3)
 				{
-						putchar(number1);
-						putchar(number2);
-						putchar(number3);
-						if (number1 == '7' && number2 == '8' &&  number3 == '9')
-						{
+					putchar(number1);
+					putchar(number2);
+					putchar(number3);
+					if (number1 == '7' && number2 == '8' &&  number3 == '9')
+					{
 						putchar('\n');
-						}
-						else
-						{
+					}
+					else
+					{
 						putchar(',');
 						putchar(' ');
-						}
+					}
 				}
+				number3++;
+			}
 	return (0);
 }
