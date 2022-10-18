2ad9c5a699e560c15dc8bed9226c8114c763d28a
Allan70 <aabere70@gmail.com>
2022-10-18 09:03:19 -0700

    102-print_comb5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,5 +8,29 @@
  */
 int main(void)
 {
+	int number1;
+	int number2;
+	int number3;
+	int number4;
+
+	for (number1 = '0'; number1 <= '9'; number1++)
+	{
+		for (number2 = '0'; number2 < '9'; number2++)
+		{
+			for (number3 = '0'; number3 <= '9'; number3++)
+			{
+				for (number4 = '0'; number4 <= '9'; number4++)
+				{
+					putchar(number1);
+					putchar(number2);
+					putchar(' ');
+					putchar(number3);
+					putchar(number4);
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
 	return (0);
 }
