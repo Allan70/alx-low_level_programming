055ce81e2bc603715242d3041c0b7b1b48a59fb1
Allan70 <aabere70@gmail.com>
2022-10-18 09:43:48 -0700

    102-print_comb5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -13,13 +13,13 @@ int main(void)
 	int number3;
 	int number4;
 
-	for (number1 = '0'; number1 <= '9'; number1++)
+	for (number1 = 48; number1 <= 57; number1++)
 	{
-		for (number2 = '0'; number2 < '9'; number2++)
+		for (number2 = 48; number2 < 57; number2++)
 		{
-			for (number3 = '0'; number3 <= '9'; number3++)
+			for (number3 = 48; number3 <= 57; number3++)
 			{
-				for (number4 = '0'; number4 <= '9'; number4++)
+				for (number4 = 48; number4 <= 57; number4++)
 				{
 					if ((( number3 + number4) > (number1 + number2) && number3 >= number1)||number1 < number2)
 					{	
@@ -28,11 +28,14 @@ int main(void)
 					putchar(' ');
 					putchar(number3);
 					putchar(number4);
-					putchar(',');
-					putchar(' ');
 					if (number1 + number2 + number3 + number4 == 227 && number1 == 57)
 					{
 						break;
+					}
+					else
+					{
+						putchar(',');
+						putchar(' ');
 					}
 				}
 				}
