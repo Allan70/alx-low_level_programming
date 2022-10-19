399364ca793bdd0de40e2243635914707207be62
Allan70 <aabere70@gmail.com>
2022-10-18 23:31:49 -0700

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _isalpha: entry point
+ * Description: checks for alphabetic character
+ * @c: The character value it receives
+ * Return: 1 if true 0 if false
+ */
+int _isalpha(int c)
+{
+	int characterUppercase;
+	int characterLowercase;
+
+	for (characterUppercase = 'A'; characterUppercase <= 'Z'; character++)
+	{
+		if (c == characterUppercase)
+		{
+			return (1);
+		}
+	}
+	for (characterLowercase = 'a'; characterLowercase <= 'z'; characterLowercase++)
+	{
+		if (c == characterLowercase)
+		{
+			return (1)
+		}
+	}
+	return (0);
+}
