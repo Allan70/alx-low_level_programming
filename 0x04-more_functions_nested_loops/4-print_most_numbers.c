8d8aedd8294276b89b853aca03352063e01c56a5
Allan70 <aabere70@gmail.com>
2022-10-21 16:02:09 -0700

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main"
+
+/**
+ * print_most_numbers - entry point
+ * Description: printnumbers 0 - 9
+ */
+
+void print_most_numbers(void)
+{
+	int number;
+
+	for (number = '0'; number >= '9'; number++)
+	{
+		if (number != '2' || number != '4')
+		{
+			_putchar(number);
+		}
+	}
+	_putchar('\n');
+}
