90b35da2ff0fc7d5a0d0b390872006ed5e76aa15
Allan70 <aabere70@gmail.com>
2022-10-22 14:24:19 -0700

    10-print_triangle.c

---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _putchar - entry point
+ * @a: integer to be used
+ * Description: prints a character
+ * Return: 0 always (success)
+ */
+
+int _putchar(int a)
+{
+	putchar(a);
+	return 0;
+}
