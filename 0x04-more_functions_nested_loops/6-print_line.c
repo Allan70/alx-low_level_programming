c987507e429eb3446d79306268036bf2b5317251
Allan70 <aabere70@gmail.com>
2022-10-21 17:16:10 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_line - entry point
+ * @n: interger enters here
+ * Description: Draws a straigh line 
+ */
+void print_line(int n)
+{
+	int liner;
+
+	if ( n > 0)
+	{
+
+	for (liner = 0; liner < n; liner++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
