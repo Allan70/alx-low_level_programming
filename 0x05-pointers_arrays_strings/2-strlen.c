1b40aafdd8b825ab90bae8c51b6de074fc134e38
Allan70 <aabere70@gmail.com>
2022-10-25 15:47:23 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen - entry point
+ * @s: Takes in a string
+ * Description: Resturs the string length
+ * Return: Returns the string length
+ */
+int _strlen(char *s)
+{
+	return (strlen(*s));
+}
