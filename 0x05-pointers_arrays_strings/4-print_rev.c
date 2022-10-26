941ea267cb6415c7a5b048cd23ed50648ae460df
Allan70 <aabere70@gmail.com>
2022-10-26 15:01:40 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/** 
+ * print_rev - entry point
+ * @s: character value to be worked on
+ * Description: Prints a string in reverse
+ */
+void print_rev(char *s)
+{
+	char holder[];
+	char counter;
+
+	for (counter = 0; s[counter] != '\0'; counter++)
+	{
+		holder[counter] = s[counter];
+	}
+
+	while (counter > 0)
+	{
+		_putchar(holder[counter]);
+		counter--;
+	}
+}
