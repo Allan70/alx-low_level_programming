e518977f5a441307ea4c22ee69ba894718234e25
Allan70 <aabere70@gmail.com>
2022-10-26 15:07:32 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,7 +10,7 @@ void print_rev(char *s)
 	char holder;
 	char counter;
 
-	for (counter = 0; s[counter] != '\0'; counter++)
+	for (counter = 0; *s[counter] != '\0'; counter++)
 	{
 		holder[counter] = s[counter];
 	}
