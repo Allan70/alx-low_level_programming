7722a1c474ef69b14a4b99fb8a296963843f05e2
Allan70 <aabere70@gmail.com>
2022-10-26 18:25:51 -0700

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -10,7 +10,7 @@ void puts_half(char *str)
 {
 	int integer;
 
-	for ( integer = 0; str[integer] != '\0'; integer++)
+	for (integer = 0; str[integer] != '\0'; integer++)
 	;
 	integer++;
 	for (integer /= 2; str[integer] != '\0';integer++)
