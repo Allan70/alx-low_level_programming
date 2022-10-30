0e290be94b20f9a759f73e8a1a78652906ed18c5
Allan70 <aabere70@gmail.com>
2022-10-29 17:15:17 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,10 +2,9 @@
 
 /**
  * *_strcat - concatinates two strings
- * @dest: string to be concatinated to
- * @str: string to concatinate
- * Description: A function that concatinates two strings.
- * Return: Returns a pointer resulting to the string dest.
+ * @str: source string to add to @dest 
+ * @dest: string to concating.
+ * Return: a pointer resulting to the string.
  */
 char *_strcat(char *dest, char *src)
 {
