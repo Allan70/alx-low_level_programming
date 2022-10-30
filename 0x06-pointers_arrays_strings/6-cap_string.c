4f7cea18e4a2fb250615fb0431db42c5ba1a1044
Allan70 <aabere70@gmail.com>
2022-10-29 19:39:26 -0700

    7-leet.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes letters of a string
+ * @str: String to capitalize
+ * Return: A pointer to the chagned string
+ */
+char *cap_string(char *str)
+{
+	int indexer;
+
+	indexeer = 0;
+
+	while (str[indexer])
+	{
+		while (!(str[indexer] >= 'a' && str[indexer] <= 'z'))
+			index++;
+
+		if (str[index -1] == ' ' ||
+		    str[index - 1] ==
+	}
+}
