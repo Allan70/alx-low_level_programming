ed51dfb555c6e8f51ef4ffd896e17496591d946c
Allan70 <aabere70@gmail.com>
2022-11-07 09:17:00 -0800

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -3,7 +3,7 @@
 int _sqrt_recursion(int n);
 int _sqrt(int n, int i);
 /**
- * _sqrt_recursion - returns the natural square root 
+ * _sqrt_recursion - returns the natural square root
  * @n: interger to be squared
  * Return: returns square root of the number
  */
@@ -15,15 +15,21 @@ int _sqrt_recursion(int n)
 	{
 		return (-1);
 	}
-	
 	if (n == 1)
 	{
 		return (1);
 	}
 
-	return _sqrt(n, i);
+	return (_sqrt(n, i));
 }
 
+/**
+ * _sqrt - returns the square root of numbers
+ * @n: numberto find sqrt for
+ * @i: testing the root
+ *
+ * Return: square root ot -1
+ */
 int _sqrt(int n, int i)
 {
 	if ((i * i) == n)
@@ -35,6 +41,6 @@ int _sqrt(int n, int i)
 	{
 		return (-1);
 	}
-	
+
 	return (_sqrt(n, i + 1));
 }
